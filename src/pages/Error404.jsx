0a82928e2f46bc3d931046209ea64d3785ee5250
Eduardo Alves <eduardoa8142@gmail.com>
2023-06-06 02:01:04 -0300

    Criando tela de ERROR. Fixes #22

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,9 @@
+import Section from "../components/Section";
+
+export default function LivroReservadoSucesso() {
+    return (
+        <Section titulo="ERROR 404">
+            <p>Página não encontrada :/</p>
+        </Section>
+    )
+}
