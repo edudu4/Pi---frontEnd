f3d7175719591d9c6029702dc50656b64b1c9cc9
Luís Nascimento <Luisrocksousa1111@gmail.com>
2023-06-09 20:19:05 -0300

    novo fonte do header. Fixes #25

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,35 +8,28 @@ import logo from '../assets/logo.svg';
 export default function Header() {
     return (
         <header className="flex justify-between items-center bg-gray-500">
-            
-                <div className="flex">
+            <div className="flex font-roboto">
                 <Link className="flex items-center" to="/">
                     <img src={logo} alt="Logo" className="w-20" />
-                    <h1 className="mx-5" >BIBLIOTECA VIRTUAL</h1>
-                    </Link>
-
-                </div>
-            <nav className="">
+                    <h1 className="mx-5 font-bold text-4xl">BIBLIOTECA VIRTUAL</h1>
+                </Link>
+            </div>
+            <nav className="font-roboto">
                 <ul className="flex gap-3">
                     <Link to="/minhasreservas">
-                        <li className="header__menu-item">Minhas Reservas</li>
+                        <li className="header__menu-item font-bold">Minhas Reservas</li>
                     </Link>
                     <li className="header__search">
-                        <input type="text"
-                            placeholder="Pesquisar Livros"
-
-                        />
-
+                        <input type="text" placeholder="Pesquisar Livros" className="font-bold" />
                         <button className="header__search-button">
                             <FontAwesomeIcon icon={faSearch} />
                         </button>
-
                     </li>
                 </ul>
             </nav>
             <Link to="/">
                 <div className="mr-5">
-                    <button className="border text-white rounded p-4">Sair</button>
+                    <button className="border text-white rounded p-4 font-bold">Sair</button>
                 </div>
             </Link>
         </header>
