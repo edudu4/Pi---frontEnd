200ac3b2369c1ade255976c836a91aa65282742f
Luís Nascimento <Luisrocksousa1111@gmail.com>
2023-06-20 20:15:50 -0300

    Corrigido o estilo da validação do formulario. Fixes #13

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,11 +1,11 @@
-import { useContext, useState } from "react" 
+import { useContext, useState } from "react"
 import { useForm } from "react-hook-form"
-import { useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import UserContext from "../contexts/UserContext"
 
 export default function LoginForm(props) {
 
-    const { register, handleSubmit, formState: {errors} } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm()
     const { handleLogin } = useContext(UserContext)
     const navigate = useNavigate()
     const [errorLogin, setErrorLogin] = useState("")
@@ -37,7 +37,7 @@ export default function LoginForm(props) {
         const { email, senha } = data
         setErrorLogin("")
         try {
-            handleLogin(email,senha)
+            handleLogin(email, senha)
             navigate("/")
         } catch (error) {
             setErrorLogin(error.message)
@@ -48,17 +48,18 @@ export default function LoginForm(props) {
         <form onSubmit={handleSubmit(onSubmit)}>
             {errorLogin && <p className="erro">{errorLogin}</p>}
             <div className="user-caixa my-2 ml-2">
-                <input type="email" id="email" name="email" {...register("email", validaEmail)}/>
-                {errors.email && <p className="erro">{errors.email.message}</p>}
+                <input type="email" id="email" name="email" {...register("email", validaEmail)} />
                 <label htmlFor="email" className='mx-2'>Email</label>
+                {errors.email && <p className="erro">{errors.email.message}</p>}
+
             </div>
             <div className="user-caixa my-2 ml-2">
-                <input type="password" id="senha" name="senha" {...register("senha", validaSenha)}/>
-                {errors.senha && <p className="erro">{errors.senha.message}</p>}
+                <input type="password" id="senha" name="senha" {...register("senha", validaSenha)} />
                 <label htmlFor="senha" className='mx-2'>Senha</label>
+                {errors.senha && <p className="erro">{errors.senha.message}</p>}
             </div>
-            <br/>
-            
+            <br />
+
             <button>Entrar</button>
         </form>
     )
